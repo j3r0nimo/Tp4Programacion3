57b9f33e84f651ad2d916e2199f469db7666af51
j3r0nimo <jerobalort@gmail.com>
2025-06-14 15:46:38 -0300

    añado 2 personas mas para cumplir con requisitos minimos de personas
    14/6

--- backend/models/PersonasModel.js
+++ backend/models/PersonasModel.js
@@ -2,6 +2,8 @@ const personas = [
     { id: 1, nombre: 'Ana', apellido: 'Gómez', edad: 28, email: 'ana@example.com' },
     { id: 2, nombre: 'Luis', apellido: 'Pérez', edad: 35, email: 'luis@example.com' },
     { id: 3, nombre: 'María', apellido: 'López', edad: 22, email: 'maria@example.com' },
+    { id: 4, nombre: 'Pedro', apellido: 'Sanchez', edad: 22, email: 'pedro@example.com' },
+    { id: 5, nombre: 'Marta', apellido: 'López', edad: 22, email: 'marta@example.com' }
 ];
 
 exports.getAll = () => personas
